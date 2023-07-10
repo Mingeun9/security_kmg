a7faaab3ca984bd321dde9e57e84d990965c3ae1
Mingeun9 <rnalsrms0908@naver.com>
2023-07-10 20:29:20 +0900

    2023-0710작업

---
+++ src/main/java/com/study/security_yeonho/SecurityYeonhoApplication.java
@@ -0,0 +1,13 @@
+package com.study.security_yeonho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityYeonhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityYeonhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/security_yeonho/SecurityYeonhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security_yeonho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityYeonhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
